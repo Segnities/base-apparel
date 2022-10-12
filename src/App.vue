4e338fb38e7eec82008e07b5f2332b93ab8ff11d
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2022-10-12 13:19:22 +0300

    project init

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="App">
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style lang="less">
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;600&display=swap');
+@import "./assets/less/variables.less";
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Josefin Sans', 'Courier New', Courier, monospace, sans-serif;
+  font-size: 16px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,4 +30,14 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.App {
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: linear-gradient(), url('./assets/img/bg-pattern-desktop.svg');
+}
+
 </style>
